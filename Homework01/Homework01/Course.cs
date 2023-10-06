e1e06375b6aafb082ee71fccc9e8e93c0ddf587e
Adrian Mincu <61752094+skpha13@users.noreply.github.com>
2023-10-06 17:31:47 +0300

    feat: done course class

--- Homework01/Homework01/Course.cs
+++ Homework01/Homework01/Course.cs
@@ -8,5 +8,27 @@ namespace Homework01
 {
 	internal class Course
 	{
+		private string name;
+		private int nrOfCredits;
+		private int nrOfHours;
+		private string professor;
+
+		public Course(string name = "Unnamed", int nrOfCredits = 0, int nrOfHours = 0, string professor = "Unknown") {
+			this.name = name;
+			this.nrOfCredits = nrOfCredits;
+			this.nrOfHours = nrOfHours;
+			this.professor = professor;
+		}	
+
+		// getters and setters
+		public string Name {  get { return name; } set { this.name = value; } }
+		public int NrOfCredits {  get { return nrOfCredits; } set { this.nrOfCredits = value; } }
+		public int NrOfHours{  get { return nrOfHours; } set { this.nrOfHours = value; } }
+		public string Professor{  get { return professor; } set { this.professor = value; } }
+
+		public override String ToString()
+		{
+			return string.Format("Course name: {0}\nProfessor: {1}\nNumber of credits: {2}\nNumber of hours: {3}\n", name, professor, nrOfCredits, nrOfHours);
+		}
 	}
 }
--- Homework01/Homework01/Program.cs
+++ Homework01/Homework01/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Homework01;
+
+Course x = new Course();
+
+Console.WriteLine(x);
